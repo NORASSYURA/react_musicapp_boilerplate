d31063a9abdf93137700363b11b1792f0047bc33
NORASSYURA <ancinersyura09@gmail.com>
2026-06-08 16:57:52 +0800

    Submission of React Assesment

--- src/components/playlist/Playlist.jsx
+++ src/components/playlist/Playlist.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import './Playlist.css';
 import Tracklist from '../tracklist/Tracklist.jsx'
 
-function Playlist() {
+function Playlist({playlistTracks, onRemoveTrack, playlistName, setPlaylistName, onsavePlaylist}) {
+
+  function handlePlaylistNameChange(event){
+  setPlaylistName(event.target.value);
+ }
+ function handleSavePlaylist(){
+  onsavePlaylist();
+ }
+ 
   return (
     <div className="Playlist">
-      <input value={'New Playlist'}/>
+      <input onChange={handlePlaylistNameChange} 
+       value={playlistName}/>
       {/* <!-- Add a TrackList component --> */}
-      <Tracklist />
-      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      <Tracklist 
+      listResults={playlistTracks} 
+      remove={true}
+      onRemoveTrack={onRemoveTrack}/>
+      <button onClick={handleSavePlaylist} className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
   )
 }
--- src/components/searchresults/SearchResults.jsx
+++ src/components/searchresults/SearchResults.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Tracklist from '../tracklist/Tracklist'
+import'./SearchResults.css';
 
-function SearchResults() {
-  return (
-    <div>SearchResults</div>
+function SearchResults({searchResults, onAddTrack}) {
+  return (    
+    <div className="SearchResults">
+      <h2>Results</h2>
+      {/* Add a TrackList component */}
+      <Tracklist 
+        listResults = {searchResults}
+        remove = {false}
+        onAddTrack = {onAddTrack}
+      />
+    </div>  
+
   )
 }
 
